66ab46ba0ab608bfe28ecff525aa86f65e9ee473
shollenbaugh0 <70164033+shollenbaugh0@users.noreply.github.com>
2020-10-19 12:23:40 -0400

    Homework 9
    added homework 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(200,118,92);
+  circle(250,250,200);
+  triangle(230,272,258,220,286,275);
+  ellipse(200,200,40,40);
+  ellipse(295,200,40,40);
+  line(200,300,265,300);
+  textSize(25);
+  text('Self-portrait',10,30);
+  text('Savanah Hollenbaugh',20,450);
+}
